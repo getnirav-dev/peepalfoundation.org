30603139009cbc55982ee8282943e1049b4d2cb6
getnirav-dev <getnirav@gmail.com>
2026-06-23 18:35:09 +0530

    Navbar Errors resolved using Antigravity on Home page, CSS and JS file updated

--- js/custom.js
+++ js/custom.js
@@ -2,44 +2,72 @@
 
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+  // PRE LOADER
+  $(window).load(function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
+
+
+  //Navigation Section
+  $('.navbar-collapse a:not(.dropdown-toggle)').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  /* Multi-level Dropdown Menu for Bootstrap */
+  $(document).on('click', '.dropdown-submenu > a', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    // Close other dropdowns
+    $('.dropdown-submenu').removeClass('open');
+    // Open this one
+    $(this).parent().addClass('open'); // Notice we changed $(this) to $(this).parent()
+    var $submenu = $(this).next('.dropdown-menu');
+    $submenu.css({
+      top: $(this).offset().top,
+      left: $(this).offset().left + $(this).outerWidth()
     });
+  });
+  ;
+
+  // Close dropdowns when clicking outside
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.navbar').length) {
+      $('.dropdown-submenu').removeClass('open');
+    }
+  });
+
+  // Owl Carousel
+  $('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    items: 1,
+    loop: true,
+    autoplay: true,
+  })
 
 
-    //Navigation Section
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
+  // PARALLAX EFFECT
+  $.stellar();
+
+
+  // SMOOTHSCROLL
+  // SMOOTHSCROLL
+  $(function () {
+    $('.navbar-default a:not(.dropdown-toggle), #home a, footer a').on('click', function (event) {
+      var $anchor = $(this);
+      var href = $anchor.attr('href');
+      // Only animate if it's a hash link pointing to a section, and not just '#'
+      if (href && href.startsWith('#') && href !== '#') {
+        $('html, body').stop().animate({
+          scrollTop: $(href).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      }
     });
+  });
 
 
-    // Owl Carousel
-    $('.owl-carousel').owlCarousel({
-      animateOut: 'fadeOut',
-      items:1,
-      loop:true,
-      autoplay:true,
-    })
 
-
-    // PARALLAX EFFECT
-    $.stellar();  
-
-
-    // SMOOTHSCROLL
-    $(function() {
-      $('.navbar-default a, #home a, footer a').on('click', function(event) {
-        var $anchor = $(this);
-          $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-          }, 1000);
-            event.preventDefault();
-      });
-    });  
-
-
-    // WOW ANIMATION
-    new WOW({ mobile: false }).init();
+  // WOW ANIMATION
+  new WOW({ mobile: false }).init();
 
 })(jQuery);
